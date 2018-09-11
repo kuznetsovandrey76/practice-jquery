240eac0a4b8c3aa981dfce02e53864d144fa4595
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-11 18:56:22 +0300

    test server

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-	echo "Hello, World";
+	$host = 'ec2-54-217-235-159.eu-west-1.compute.amazonaws.com';
+	$user = 'wwagzkrthsjslb';
+	$pass = 'f037a36e6e2452e351f9e88446df480d013dcc943b5de972a461682fb8811dd9';
+	$db   = 'd6u19tp8jutgfb';
+	$connection = pg_connect ('host=$host port=5432 dbname=$db user=$user password=$pass');
+	
+	if (!$connection) {
+		die('Не удалось соединиться с базой данных');
+	}
+
+	$array = pg_query($connection, "SELECT * FROM blog;");	
+	pg_close($connection);
+	
+	pg_fetch_all( $array );
 ?>
